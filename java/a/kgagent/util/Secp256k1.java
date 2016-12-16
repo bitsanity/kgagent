f932fc2b15b91d08689ed0a363c7ad2dec097ba3
bitsanity <brydiver@gmail.com>
2016-12-16 09:45:53 -0800

    remove hardcoded path

--- java/a/kgagent/util/Secp256k1.java
+++ java/a/kgagent/util/Secp256k1.java
@@ -2,11 +2,9 @@ package a.kgagent.util;
 
 public class Secp256k1
 {
-  // expect libkeymaster.so includes libsecp256k1.so all x-compiled for
-  // correct target architecture
   static
   {
-    System.load( "/home/skull/secp256k1/.libs/libsecp256k1.so" );
+    System.loadLibrary( "secp256k1" );
     System.loadLibrary( "kgagent" );
   }
 
